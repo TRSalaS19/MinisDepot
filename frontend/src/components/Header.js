9bcc0e2438f671c95c5eea684ab3fe0664e104ca
Gustavo Salas <gustavosalas@Mac-mini.local>
2021-07-17 08:50:21 -0700

    implemented the react router and installed react router dom to be able to link areas of the page without reloading

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Navbar, Nav, Container} from 'react-bootstrap';
+import { LinkContainer} from 'react-router-bootstrap';
 import headerImg from '../img/header/header.png';
 
 
@@ -9,15 +10,21 @@ const Header = () => {
     <header>
       <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>
-            <img className='headerLogo mx-2' src={headerImg} alt='mini' />
-            Mini's Depot
-          </Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img className='headerLogo mx-2' src={headerImg} alt='mini' />
+              Mini's Depot
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
-              <Nav.Link href='/login'><i className="fas fa-user"></i> Login</Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link><i className="fas fa-user"></i> Login</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import {Card,Carousel} from 'react-bootstrap';
 import Rating from './Rating';
 
@@ -8,22 +9,26 @@ const Product = ({product}) => {
 
   return (
     <Card className='my-3 p-3 rounded'>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="w-100"
-              src={imgOne}
-              style={{width:"200px", height:"200px"}}
-              alt="First slide"
-            />
+        <Carousel hover='true' interval={5000} >
+          <Carousel.Item >
+            <Link to={`/product/${product._id}`}>
+              <img
+                className="w-100 rounded"
+                src={imgOne}
+                style={{width:"200px", height:"200px"}}
+                alt="First slide"
+              />
+            </Link>
           </Carousel.Item>
           <Carousel.Item>
+          <Link to={`/product/${product._id}`}>
             <img
-              className="w-100"
+              className="w-100 rounded"
               src={imgTwo}
               style={{width:"200px", height:"200px"}}
               alt="Second slide"
             />
+          </Link>
           </Carousel.Item>
         </Carousel>
       <Card.Body>
